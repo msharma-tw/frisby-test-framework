cdefc904bd2c69fc75a4c0db4471d85b5ac39a2b
msharma-tw <monika.sharma@thoughtworks.com>
2020-10-08 22:25:43 +0530

    update | added environment variables

--- __tests__/user_service/get_test_spec.js
+++ __tests__/user_service/get_test_spec.js
@@ -3,7 +3,9 @@ const config = require('../../resources/config');
 
 //GET call with assertions on header and response body
 it("should be UP for heartbeat", function () {
-  
+
+  console.log(process.env.FR_TEST_TOKEN);
+
   return frisby
     .get(config.cm_base_url + "/v0.5/heartbeat")
     .expect("status", 200)
